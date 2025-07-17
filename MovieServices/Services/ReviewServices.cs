a77583b82211ea84b693777f1ece3e9d388b5f78
a21jeaha <hisui86@gmail.com>
2025-07-17 09:49:05 +0200

    ReviewServices now throw custom exceptions

--- MovieServices/Services/ReviewServices.cs
+++ MovieServices/Services/ReviewServices.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MovieCore.DomainContracts;
 using MovieCore.Models.DTOs.ReviewDTOs;
+using MovieCore.Models.Exceptions;
 using ServicesContracts.Contracts;
 
 namespace MovieServices.Services;
@@ -25,19 +26,8 @@ public class ReviewServices : IReviewServices
 	{
 		var movieExists = await _unitOfWork.Movies.AnyAsync(movieId);
 
-		if (movieExists == false)
-		{
-			//return Problem(
-			//	statusCode: StatusCodes.Status404NotFound,
-			//	title: "Invalid movie ID",
-			//	detail: $"No movie with ID {movieId} was found.",
-			//	instance: HttpContext.Request.Path
-			//);
-
-			// ToDo : Create a custom exception and handle this exception in program.cs 
-			throw new ArgumentNullException($"No movie with ID {movieId} was found.");
-		}
-
+		if (movieExists == false) throw new MovieNotFoundException(movieId);
+		
 		return await _unitOfWork.Reviews.GetAllReviewsForMovieAsync(movieId, changeTracker: false);
 	}
 }

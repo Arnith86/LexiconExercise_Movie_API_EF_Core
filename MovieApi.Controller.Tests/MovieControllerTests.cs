89471e0bef6feabdee31553364d2ac72b3ff7e9c
a21jeaha <hisui86@gmail.com>
2025-08-03 12:30:23 +0200

    Added Unit test to MovieController GetMovie expected  exception throw

--- MovieApi.Controller.Tests/MovieControllerTests.cs
+++ MovieApi.Controller.Tests/MovieControllerTests.cs
@@ -5,47 +5,72 @@ using MovieApi.Controller.Tests.DatabaseFixtures;
 using MovieApi.Controllers;
 using MovieCore.Models.DTOs.MovieDtos;
 using MovieCore.Models.Entities;
+using MovieCore.Models.Exceptions;
 using Services.Contracts;
 
 namespace MovieApi.Controller.Tests
 {
 	public class MovieControllerTests
 	{
+		private MoviesController _sut;
 		private Mock<IServiceManager> _mockServiceManager;
-		private MoviesController _controller;
 		private MovieDatabaseFixture _movieDatabaseFixture;
+
+		private const int _c_MoviesCreateDefault_10 = 10;
+		private const int _c_MovieCreate_SingleMovie_1 = 1;
+
+		private const int _c_MovieId_1 = 1;
 
 		public MovieControllerTests()
 		{
 			_mockServiceManager = new Mock<IServiceManager>();
-			_controller = new MoviesController(_mockServiceManager.Object);
+			_sut = new MoviesController(_mockServiceManager!.Object);
 			_movieDatabaseFixture = new MovieDatabaseFixture();
 		}
 
 		[Fact]
-		public async Task GetMovie_ShouldReturn_StatusCode200OkAsync()
+		public async Task GetMovie_ShouldReturn_StatusCode200OkAsync_MovieWithGenreDto()
 		{
 			// Arrange 
 			var response = await GetMovieWithGenre();
 
 			_mockServiceManager
-				.Setup(sm => sm.MovieServices.GetMovieAsync(1))
+				.Setup(sm => sm.MovieServices.GetMovieAsync(_c_MovieCreate_SingleMovie_1))
 				.ReturnsAsync(response);
 
 			// Act 
-			var result = await _controller.GetMovie(1);
-			var okResult = result.Result as OkObjectResult;
+			var result = await _sut.GetMovie(_c_MovieId_1);
+			var resultType = result.Result as OkObjectResult;
 
 			// Assert 
-			Assert.IsType<OkObjectResult>(okResult);
-			Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
-			Assert.Equal(response, okResult.Value);
+			Assert.IsType<OkObjectResult>(resultType);
+			Assert.Equal(StatusCodes.Status200OK, resultType.StatusCode);
+			Assert.Equal(response, resultType.Value);
+		}
+
+
+		/// <summary>
+		/// This test ensures that the controller correctly propagates the exception from the service layer
+		/// when an invalid movie ID is provided.
+		/// </summary>
+		[Fact]
+		public async Task GetMovie_ShouldReturn_StatusThrow_MovieNotFoundException()
+		{
+			// Arrange
+			_mockServiceManager
+				.Setup(sm => sm.MovieServices.GetMovieAsync(It.IsAny<int>()))
+				.ThrowsAsync(new MovieNotFoundException(_c_MovieId_1));
+
+			// Act & Assert
+			await Assert.ThrowsAsync<MovieNotFoundException>(() => _sut.GetMovie(_c_MovieId_1));
+		}
+
+			_mockPaginationMetaDate.SetupGet(pmd => pmd.hasNext).Returns(true);
 		}
 
 		private async Task<MovieWithGenreDto> GetMovieWithGenre()
 		{
 			var movies = await _movieDatabaseFixture.GetMovieWithGenre();
-
 			return movies.First();
 		}
 

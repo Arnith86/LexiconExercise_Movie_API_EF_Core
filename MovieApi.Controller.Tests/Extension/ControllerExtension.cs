2088fc9e415fe70019388ded3ac249b14dd37f55
a21jeaha <hisui86@gmail.com>
2025-08-03 12:31:30 +0200

    added unit test moviecontroller GetMovies Positive outcome
    Check if respons is positive an that pagination has been performed

---
+++ MovieApi.Controller.Tests/Extension/ControllerExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.Controller.Tests.Extension;
+
+public static class ControllerExtension
+{
+	public static void SetupDefaultHttpContext(this ControllerBase controller)
+	{
+		var httpContext = new DefaultHttpContext();
+
+		var controllerContext = new ControllerContext
+		{
+			HttpContext = httpContext
+		};
+
+		controller.ControllerContext = controllerContext;
+	}
+}
--- MovieApi.Controller.Tests/MovieControllerTests.cs
+++ MovieApi.Controller.Tests/MovieControllerTests.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿// Ignore Spelling: Dto
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using MovieApi.Controller.Tests.DatabaseFixtures;
+using MovieApi.Controller.Tests.Extension;
 using MovieApi.Controllers;
+using MovieCore.DomainContracts.RequestParameters;
 using MovieCore.Models.DTOs.MovieDtos;
 using MovieCore.Models.Entities;
 using MovieCore.Models.Exceptions;
@@ -14,7 +18,10 @@ namespace MovieApi.Controller.Tests
 	{
 		private MoviesController _sut;
 		private Mock<IServiceManager> _mockServiceManager;
+		private MovieRequestParameters _movieRequestParameter;
 		private MovieDatabaseFixture _movieDatabaseFixture;
+		private Mock<IPageList<VideoMovie>> _mockPageList;
+		private Mock<IPaginationMetaData> _mockPaginationMetaDate;
 
 		private const int _c_MoviesCreateDefault_10 = 10;
 		private const int _c_MovieCreate_SingleMovie_1 = 1;
@@ -25,7 +32,10 @@ namespace MovieApi.Controller.Tests
 		{
 			_mockServiceManager = new Mock<IServiceManager>();
 			_sut = new MoviesController(_mockServiceManager!.Object);
+			_movieRequestParameter = new MovieRequestParameters();
 			_movieDatabaseFixture = new MovieDatabaseFixture();
+			_mockPageList = new Mock<IPageList<VideoMovie>>();
+			_mockPaginationMetaDate = new Mock<IPaginationMetaData>();
 		}
 
 		[Fact]
@@ -65,6 +75,50 @@ namespace MovieApi.Controller.Tests
 			await Assert.ThrowsAsync<MovieNotFoundException>(() => _sut.GetMovie(_c_MovieId_1));
 		}
 
+		/// <summary>
+		/// This test verifies that when a valid movie request with pagination parameters is made,
+		/// the controller returns a 200 OK response containing the expected paginated list of 
+		/// <see cref="MovieWithGenreDto"/> items and sets the "X-Pagination" response header.
+		/// </summary>
+		[Fact]
+		public async Task GetMovies_ShouldReturn_StatusCode200OkAsync_PaginatedIEnumerableOf_MovieWithGenreDto()
+		{
+			// Arrange 
+			var moviesWithGenre = await GetMoviesWithGenre();
+			SetupValidPaginationMetaData();
+
+			_movieRequestParameter.PageNumber = 1;
+			_movieRequestParameter.PageSize = 5;
+
+			_mockServiceManager
+				.Setup(sm => sm.MovieServices.GetAllMoviesAsync(_movieRequestParameter, false))
+				.ReturnsAsync((moviesWithGenre, _mockPaginationMetaDate.Object));
+
+			_sut.SetupDefaultHttpContext(); // want to access here but cannot 
+
+			// Act 
+			var result = await _sut.GetMovies(_movieRequestParameter);
+			var resultType = result.Result as OkObjectResult;
+
+			// Assert 
+			Assert.True(_sut.HttpContext.Response.Headers.ContainsKey("X-Pagination"));
+			var headerValue = _sut.HttpContext.Response.Headers["X-Pagination"].ToString();
+			Assert.False(string.IsNullOrEmpty(headerValue));
+			Assert.Contains("\"CurrentPage\":1", headerValue);
+			Assert.Contains("\"PageSize\":5", headerValue);
+
+			Assert.IsType<OkObjectResult>(resultType);
+			Assert.Equal(StatusCodes.Status200OK, resultType.StatusCode);
+			Assert.Equal(moviesWithGenre, resultType.Value);
+		}
+
+		private void SetupValidPaginationMetaData()
+		{
+			_mockPaginationMetaDate.SetupGet(pmd => pmd.CurrentPage).Returns(1);
+			_mockPaginationMetaDate.SetupGet(pmd => pmd.PageSize).Returns(5);
+			_mockPaginationMetaDate.SetupGet(pmd => pmd.TotalItemCount).Returns(_c_MoviesCreateDefault_10);
+			_mockPaginationMetaDate.SetupGet(pmd => pmd.TotalPages).Returns(2);
+			_mockPaginationMetaDate.SetupGet(pmd => pmd.hasPrevious).Returns(false);
 			_mockPaginationMetaDate.SetupGet(pmd => pmd.hasNext).Returns(true);
 		}
 
@@ -74,6 +128,7 @@ namespace MovieApi.Controller.Tests
 			return movies.First();
 		}
 
-
+		private async Task<IEnumerable<MovieWithGenreDto>> GetMoviesWithGenre() =>
+			await _movieDatabaseFixture.GetMovieWithGenre(10);
 	}
 }
